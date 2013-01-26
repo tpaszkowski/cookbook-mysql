ea67f89fc029a477cc218e33d0593326aa316e68
Alexander Khalkuziev <akhalkuziev@mirantis.com>
2013-01-26 18:09:56 +0400

    Fix syntax error in module declaration.

--- libraries/utils.rb
+++ libraries/utils.rb
@@ -1,24 +1,23 @@
 #!/usr/bin/env ruby
 
-Module MG
-def check_state_attr(server,attrs={})
-  attrib = attrs["attr"]
-  a_key = attrs["key"]
-  a_var = attrs["var"]
-  a_time = attrs["timeout"]
-  until server.attribute?(attrib)&&server[attrib].key?(a_key)&&server[attrib][a_key]==a_var do
-    if (Time.now.to_f-sttime)>=a_time
-      Chef::Application.fatal! "Timeout exceeded while node #{server.name} syncing.."
-    else
-      Chef::Log.info "Waiting while node #{server.name} syncing.."
-      sleep 10
-      server = search(:node, "name:#{server.name} AND chef_environment:#{node.chef_environment}")[0]
+module MG
+  def check_state_attr(server,attrs={})
+    attrib = attrs["attr"]
+    a_key = attrs["key"]
+    a_var = attrs["var"]
+    a_time = attrs["timeout"]
+    until server.attribute?(attrib)&&server[attrib].key?(a_key)&&server[attrib][a_key]==a_var do
+      if (Time.now.to_f-sttime)>=a_time
+        Chef::Application.fatal! "Timeout exceeded while node #{server.name} syncing.."
+      else
+        Chef::Log.info "Waiting while node #{server.name} syncing.."
+        sleep 10
+        server = search(:node, "name:#{server.name} AND chef_environment:#{node.chef_environment}")[0]
+      end
     end
+    true
   end
-  true
 end
-end
-
 
 class Chef::Recipe
   include MG
